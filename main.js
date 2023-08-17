6acc0c02e4dd5db14347847cad9e9906d9c59cb3
fifi21618 <fiona.hanura@gmail.com>
2023-08-18 02:25:09 +0300

    Third commit

--- main.js
+++ main.js
@@ -2,16 +2,14 @@
 var tablinks = document.getElementsByClassName("tab-link");
 var tabcontents = document.getElementsByClassName("tab-content");
 
-function openTab(tabname, event) {
-    for (const tablink of tablinks) {
-        tablink.classList.remove("active-link");
+function openTab(tabname) {
+    for ( tablink of tablinks) {
+        tablinks.classList.remove("active-link");
     }
-
-    for (const tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
+    for ( tabcontent of tabcontents) {
+        tabcontents.classList.remove("active-tab");
     }
-    
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("tab-content");
+   event.currentTarget.classList.add("active-link");
+   document.getElementById(tabname).classList.add("tab-content");
 }
 
