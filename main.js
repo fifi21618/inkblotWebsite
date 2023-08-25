711ffc2f6e05ee6bf0d54a4e17196700c9412f38
fifi21618 <fiona.hanura@gmail.com>
2023-08-26 02:59:12 +0300

    about issue solved,addition of content sample,nav-links

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
 
-var tablinks = document.getElementsByClassName("tab-link");
-var tabcontents = document.getElementsByClassName("tab-content");
 
-function openTab(tabname) {
-    for ( tablink of tablinks) {
-        tablinks.classList.remove("active-link");
-    }
-    for ( tabcontent of tabcontents) {
-        tabcontents.classList.remove("active-tab");
-    }
-   event.currentTarget.classList.add("active-link");
-   document.getElementById(tabname).classList.add("tab-content");
-}
 
